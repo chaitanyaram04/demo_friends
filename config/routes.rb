09b4c0ba56af3e1555a3d6c79615d8289001240b
chaitanyaram04 <chaitanya.ram@vegrow.in>
2025-01-04 19:53:38 +0530

    CRUD done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :friends
   #get 'home/index'
   root 'home#index'
+  get 'home/about'
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+
+ActiveRecord::Schema[7.1].define(version: 2025_01_04_040957) do
+  create_table "friends", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
